acd1dc1b48645a03eb16630d03d21dd2422a0e61
Lans <aziellan27@gmail.com>
2026-03-11 20:36:56 +0800

    Removed the Console Arguement

--- DownloadSorter/Program.cs
+++ DownloadSorter/Program.cs
@@ -9,22 +9,10 @@ namespace DownloadSorter
 		static void Main(string[] args)
 		{
 			UIManager uiManager = new UIManager();
-			if (args.Contains("Console", StringComparer.OrdinalIgnoreCase))
-			{
+			//if (args.Contains("Console", StringComparer.OrdinalIgnoreCase))
+			//{
 				uiManager.ConsoleDisplay();
-			}
+			//}
 		}
 	}
 }
-//NavigationManager Navman = new NavigationManager();
-//SortManagment sortMan = new SortManagment();
-
-//Console.Title = "DownloadSorter";
-//Console.WriteLine("Download Sorter" + "[" + "v" + Assembly.GetExecutingAssembly().GetName().Version + "]");
-//Thread.Sleep(1000);
-//if(!File.Exists("Sorts.json"))
-//{
-//    sortMan.DownloadManager(true);
-//}
-//Navman.MainNavigation(true);
-//Console.ReadLine();
